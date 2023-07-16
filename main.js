744d97d0ff47e73f9014e09e32fe8bf3fc1e71bf
Maggie <96947247+MaggieLOL@users.noreply.github.com>
2023-07-16 21:49:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function resolveNetease(text){
+    var template = "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=330 height=86 src=\"//music.163.com/outchain/player?type=2&id=#ID#&auto=1&height=66\"></iframe>"    
+    if(text.startsWith("https://music.163.com/#/song?id=")){
+        var songid = parseInt(text.slice(32));
+    }else{
+        return false
+    }
+    return template.replace("#ID#",songid)
+}
+function scrollToEnd(){
+    window.scrollTo(0,document.body.scrollHeight);
+}
+function neteaseHook(...args){
+    var rawel = args[0][0].childNodes[2];
+    var parsed=resolveNetease(rawel.innerText);
+    if(!!parsed){
+        rawel.innerHTML=parsed
+    }
+    setTimeout(scrollToEnd,200);
+}
+
+
+hook.register("after","pushmessage",neteaseHook)
