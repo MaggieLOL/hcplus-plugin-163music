eb0a82e5a70c39c20a47e728d9754c647a3831f1
0x24a <96947247+MaggieLOL@users.noreply.github.com>
2024-01-20 22:02:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function resolveNetease(text){
     var template = "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=330 height=86 src=\"https://music.163.com/outchain/player?type=2&id=#ID#&auto=1&height=66\"></iframe>"    
-    if(text.startsWith("https://music.163.com/#/song?id=")){
+    if(text.startsWith("https://music.163.com/song?id=")){
         var songid = parseInt(text.slice(32));
     }else{
         return false
